4a9d827557caee2130d2f01b43599b02b0c03c97
saud sayyed <155637796+Saud12131@users.noreply.github.com>
2024-10-26 03:18:18 +0530

    started working on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
 
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Intro from "./pages/intro";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Home from "./pages/home";
 function App() {
   return (
-    <>
-      <h3>hello dev</h3>
-    </>
-
-  )
+<BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Intro />} />
+        <Route  path="/home" element={<Home />} />
+        <Route  path="/login" element={<Login />} />
+        <Route  path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Intro() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">WELCOME TO TODO!</h1>
+        </div>
+    )
+}
